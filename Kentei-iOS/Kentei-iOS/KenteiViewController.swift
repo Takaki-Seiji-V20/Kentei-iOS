7d76119d16827568784bc341cd8cd4c9b3ad9ab3
john <vsn2047508@gmail.com>
2017-06-14 16:30:37 +0900

    add title画面

---
+++ Kentei-iOS/Kentei-iOS/KenteiViewController.swift
@@ -0,0 +1,35 @@
+//
+//  KenteiViewController.swift
+//  Kentei-iOS
+//
+//  Created by dev01 on 2017/06/14.
+//  Copyright © 2017年 dev01. All rights reserved.
+//
+
+import UIKit
+
+class KenteiViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Kentei-iOS/Kentei-iOS/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Kentei-iOS
+//
+//  Created by dev01 on 2017/06/14.
+//  Copyright © 2017年 dev01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    //@IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var creditLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //CSVファイル名を引数にしてloadCSVメソッドを使用し、CSVファイルを読み込む
+        let csvArray = loadCSV("start")
+        //logoImageViewに画像を設定
+        logoImageView.image = UIImage(named: csvArray[0])
+        //titleLabelにアプリ名を設定
+        titleLabel.text = csvArray[1]
+        //bodyTextViewにアプリ説明文を設定
+        bodyTextView.text = csvArray[2]
+        //ボタンの文字を白色に変更
+        //startButton.setTitleColor(UIColor.white, for: .normal)
+        //creditLabelにクレジットを設定
+        creditLabel.text = csvArray[3]
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func loadCSV(_ fileName :String) -> [String]{
+        
+
+        //CSVファイルのデータを格納するStrig型配列
+        var csvArray:[String] = []
+        //引数filnameからCSVファイルのパスを設定
+        let csvBundle = Bundle.main.path(forResource: fileName, ofType: "csv")!
+        do {
+            
+            //csvBundleからファイルを読み込み、エンコーディングしてcsvDataに格納
+            let csvData = try String(contentsOfFile: csvBundle,encoding: String.Encoding.utf8)
+            //改行コードが"\r"の場合は"\n"に置換する
+            let lineChange = csvData.replacingOccurrences(of: "\r", with: "\n")
+            //"\n"の改行コードで要素を切り分け、配列csvArrayに格納する
+            csvArray = lineChange.components(separatedBy: "\n")
+        }catch{
+            print("エラー")
+        }
+        return csvArray     //戻り値の配列csvArray
+    }
+
+
+}
+
