802d05b96b69cdec3d298a9aa8bdd579ad16f469
john <vsn2047508@gmail.com>
2017-06-15 15:07:54 +0900

    update kenteiview scoreview

--- Kentei-iOS/Kentei-iOS/ScoreViewController.swift
+++ Kentei-iOS/Kentei-iOS/ScoreViewController.swift
@@ -7,13 +7,84 @@
 //
 
 import UIKit
+//Socialフレームワークをインポート
+import Social
 
 class ScoreViewController: UIViewController {
-
+    
+    //KenteiViewControllerの正解数を受け取るメンバ変数
+    var correct = 0
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var judgeImageView: UIImageView!
+    @IBOutlet var goukakuTimesLabel: UILabel!
+    @IBOutlet var rankLabel: UILabel!
+    @IBOutlet var kenteiBackBtn: UIButton!
+    
+    var goukakuTimes = 0   //合格回数を格納する変数
+    var rankString = "ビギナー"   //称号変数。初期値はビギナー
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //合格回数を保存するUserDefaults
+        let goukakuUd = UserDefaults.standard
+        //合格回数をgoukakuというキー値で変数goukakuTimesに格納
+        goukakuTimes = goukakuUd.integer(forKey: "goukaku")
+        //正解数を表示
+        scoreLabel.text = "正解数は\(correct)問です。"
+        //合格・不合格を判定
+        if correct >= 7{
+            judgeImageView.image = UIImage(named: "Goukaku.png")
+            goukakuTimes += 1            //合格回数をカウントアップ
+            //goukakuキー値を使って合格回数(goukakuTimes)を保存
+            goukakuUd.set(goukakuTimes, forKey: "goukaku")
+        }else{
+            judgeImageView.image = UIImage(named: "Fugoukaku.png")
+        }
+        //合格回数を表示
+        goukakuTimesLabel.text = "合格回数は\(goukakuTimes)回です。"
+        //合格回数によってランクを決定
+        if goukakuTimes >= 50{
+            rankString = "達人"
+        }else if goukakuTimes >= 40{
+            rankString = "師匠"
+        }else if goukakuTimes >= 30{
+            rankString = "師範代"
+        }else if goukakuTimes >= 20{
+            rankString = "上級者"
+        }else if goukakuTimes >= 10{
+            rankString = "ファン"
+        }else if goukakuTimes >= 0{
+            rankString = "ビギナー"
+        }
+        //ランクラベルに称号を設定
+        rankLabel.text = "ランクは\(rankString)！"
+    }
 
-        // Do any additional setup after loading the view.
+    //Facebook投稿メソッド
+    @IBAction func postFacebook(_ sender: AnyObject) {
+        //Facebook投稿用インスタンスを作成
+        let fbVC:SLComposeViewController = SLComposeViewController  (forServiceType: SLServiceTypeFacebook)!
+        //投稿テキストを設定
+        fbVC.setInitialText("三浦のおやさい検定：私は\(rankString)。合格回数は\(goukakuTimes)回です。")
+        //投稿画像を設定
+        fbVC.add(UIImage(named: "icon.png"))
+        //投稿用URLを設定
+        fbVC.add(URL(string: "http://onthehammock.com/app/5783"))
+        //投稿コントローラーを起動
+        self.present(fbVC, animated: true,   completion: nil)
+    }
+    //Twitter投稿メソッド
+    @IBAction func postTwitter(_ sender: AnyObject) {
+        //Twitter投稿用インスタンスを作成
+        let twVC:SLComposeViewController = SLComposeViewController  (forServiceType: SLServiceTypeTwitter)!
+        //投稿テキストを設定
+        twVC.setInitialText("三浦のおやさい検定：私は\(rankString)。合格回数は\(goukakuTimes)回です。")
+        //投稿画像を設定
+        twVC.add(UIImage(named: "icon.png"))
+        //投稿用URLを設定
+        twVC.add(URL(string: "http://onthehammock.com/app/5783"))
+        //投稿コントローラーを起動
+        self.present(twVC, animated: true,   completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
